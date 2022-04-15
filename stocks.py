74e508a9450ac2a1fe40f86bf637e44dfb54ec99
XDevant <59935400+XDevant@users.noreply.github.com>
2022-04-15 10:52:29 +0200

    Delete stocks.py

--- stocks.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from models import Share
-
-df = pd.read_csv("./dataset0_Python+P7.csv", delimiter=",")
-
-def load_stocks(index):
-    df = pd.read_csv(f"./dataset{index}_Python+P7.csv", delimiter=",")
-    stocks = [Share(i, df) for i in range(len(df))]
-    return stocks
